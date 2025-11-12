ae428a107622af64b01c18e8a7ee83fa20ca4186
rosgab0910 <gabrieloliveirarosa@colegiopaulodetarso.com.br>
2025-11-12 14:54:59 +0000

    commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Hello, Flask !!</h1>'
+    
+@app.route('/versao')
+def versao():
+    versao = "1.1.0"
+    return f'App v{versao}'
+
+@app.route('/saudar/<nome>')
+def saudar(nome):
+    nome_capitalizado = nome.capitalize()
+    return f'Olá, {nome_capitalizado}!'
+
+@app.route('/quadrado/<int:n>')
+def quadrado(n):
+    resultado = n ** 2
+    return f'{n}² = {resultado}'    
